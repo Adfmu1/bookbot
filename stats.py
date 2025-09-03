8821c0f1453e1e2d7fca3b0e2d639ad28d58862e
Adfmu1 <adfmu3@gmail.com>
2025-09-03 14:32:24 +0200

    add function counting each char in word

--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def get_num_words(book_content: str) -> None:
     word_count = len([word for word in book_content.split()])
     print(f"{word_count} words found in the document")
+    
+def get_char_words(book_content: str) -> dict:
+    ind_char_count = {}
+    
+    for word in book_content.split():
+        for char in word.lower():
+            if char not in ind_char_count:
+                ind_char_count[char] = 1
+            else:
+                ind_char_count[char] += 1
+                
+    return ind_char_count
